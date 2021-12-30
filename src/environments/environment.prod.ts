0b993fc0b4591597ac7962a641c82947a6b4ebb2
RomarioJPMenezes <34045950+RomarioJPMenezes@users.noreply.github.com>
2021-12-30 22:35:41 +0000

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api-genealogia.herokuapp.com',
+  tokenAllowedDomains: [ /api-genealogia.herokuapp.com/ ],
+  tokenDisallowedRoutes: [/\/oauth\/token/]
 };
