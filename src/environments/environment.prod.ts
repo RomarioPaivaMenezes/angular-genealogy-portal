08a1b7a791570aa71bf4ad162834419fd0e819ad
RomarioJPMenezes <34045950+RomarioJPMenezes@users.noreply.github.com>
2021-12-31 13:55:00 +0000

    asdasd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-genealogia.herokuapp.com',
-  tokenAllowedDomains: [ /api-genealogia.herokuapp.com/ ],
+  tokenAllowedDomains: [ /api-genealogia.herokuapp/ ],
   tokenDisallowedRoutes: [ /\/oauth\/token/]
 }
