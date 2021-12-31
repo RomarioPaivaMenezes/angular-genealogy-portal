0388b36c05691450a459789c6676ed24f3b78786
RomarioJPMenezes <34045950+RomarioJPMenezes@users.noreply.github.com>
2021-12-31 14:44:19 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-genealogia.herokuapp.com',
-  tokenAllowedDomains: [ new RegExp('api-genealogia.herokuapp') ],
+  tokenAllowedDomains: [ new RegExp('api-genealogia.herokuapp.com') ],
   tokenDisallowedRoutes: [ new RegExp('/\/oauth\/token')]
 }
