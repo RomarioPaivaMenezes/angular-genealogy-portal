513640f7a1c16a550d413ad981123a20124a62d3
RomarioJPMenezes <34045950+RomarioJPMenezes@users.noreply.github.com>
2021-12-31 12:37:24 +0000

    sadasd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-genealogia.herokuapp.com',
-  tokenAllowedDomains: [ /agile-fjord-35937.herokuapp.com/ ],
+  tokenAllowedDomains: [ /agile-fjord-35937.herokuapp.com:4200/ ],
   tokenDisallowedRoutes: [/\/oauth\/token/]
 }
