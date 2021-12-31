883d859a8fd8d7ec1d2539bc96553b6b71d47bf1
RomarioJPMenezes <34045950+RomarioJPMenezes@users.noreply.github.com>
2021-12-31 13:25:53 +0000

    sadsad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-genealogia.herokuapp.com',
-  tokenAllowedDomains: [ new RegExp('agile-fjord-35937.herokuapp.com') ],
+  tokenAllowedDomains: [ new RegExp('agile-fjord-35937.herokuapp') ],
   tokenDisallowedRoutes: [ new RegExp('\/oauth\/token')]
 }
